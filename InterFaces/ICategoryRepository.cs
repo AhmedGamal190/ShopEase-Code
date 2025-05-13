e4777769810203a90bd3efcecfafdb93bc6c12ec
AhmedGamal190 <125511451+AhmedGamal190@users.noreply.github.com>
2025-05-13 12:08:31 +0300

    Add files via upload

---
+++ InterFaces/ICategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using ProductCategory.Models;
+
+namespace ProductCategory.InterFaces
+{
+    public interface ICategoryRepository : IGeneralRepository<Category>
+    {
+        IEnumerable<Product> GetProductByCategory( string name , int id );
+        public IEnumerable<Category> GetCategoryByName(string name);
+    }
+}
---
+++ InterFaces/IProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using ProductCategory.Models;
+
+namespace ProductCategory.InterFaces
+{
+    public interface IProductRepository : IGeneralRepository<Product>
+    {
+        IEnumerable<Product> GetByName(string name , int CategoryId);
+        IEnumerable<Product> ProductsWithCategory();
+        public IEnumerable<Product> GetProductByName(string name);
+    }
+}
