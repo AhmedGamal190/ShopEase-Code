dd129b48aa6566a0dbb25c5809cf2e2164b90b98
AhmedGamal190 <125511451+AhmedGamal190@users.noreply.github.com>
2025-05-13 12:18:11 +0300

    Add files via upload

---
+++ Repositories/ProductRepository.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductCategory.InterFaces;
+using ProductCategory.Models;
+
+namespace ProductCategory.Repositories
+{
+    public class ProductRepository : GeneralRepository<Product>, IProductRepository
+    {
+        public ProductRepository(SystemContext dB) : base(dB)
+        {
+        }
+
+        public IEnumerable<Product> GetProductByName(string name)
+        {
+            return DB.Products.Where(x => x.Name == name).ToList();
+        }
+        public IEnumerable<Product> GetByName(string name, int CategoryId)
+        {
+            List<Product> Product = new List<Product>();
+            if (CategoryId > 0)
+            {
+                Product = DB.Products.Where(x => x.Name.StartsWith(name) && x.CategoryId == CategoryId).ToList();
+            }
+            else
+                 Product = DB.Products.Where(x => x.Name.StartsWith(name) ).ToList();
+            return Product;
+        }
+
+
+        public IEnumerable<Product> ProductsWithCategory()
+        {
+            var products = DB.Products.Include(x => x.Category).ToList();
+            return products;
+        }
+    }
+}
